1b4011955961b0520f40cf69fd2dac974973bc1d
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2017-08-07 21:22:52 +0700

    Story 02 - Add API call to get all prices for 1 day for 1 pair of source-destination

---
+++ app/src/main/java/com/huynd/skyobserver/models/PricePerDayResponse.java
@@ -0,0 +1,20 @@
+package com.huynd.skyobserver.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by HuyND on 8/7/2017.
+ */
+
+public class PricePerDayResponse {
+    @SerializedName("departureDate")
+    private int departureDate;
+
+    @SerializedName("flightNumber")
+    private String flightNumber;
+
+    @SerializedName("priceList")
+    private List<PricePerDay> priceList;
+}
--- app/src/main/java/com/huynd/skyobserver/services/PricesAPI.java
+++ app/src/main/java/com/huynd/skyobserver/services/PricesAPI.java
@@ -1,8 +1,20 @@
 package com.huynd.skyobserver.services;
 
+import com.huynd.skyobserver.models.PricePerDayResponse;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Path;
+
 /**
  * Created by HuyND on 8/6/2017.
  */
 
 public interface PricesAPI {
+    @GET("{carrier}/{srcPort}/{dstPort}")
+    Call<List<PricePerDayResponse>> getPricePerDay(@HeaderMap Map<String, String> headers, @Path("carrier") String carrier);
 }
