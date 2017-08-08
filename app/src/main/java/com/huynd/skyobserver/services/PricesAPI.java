7b61f1d04d14c2126ebb375a0f3cc7910ecc7bc6
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2017-08-08 21:17:30 +0700

    Story 02 - Modify API call to send POST request with data

--- app/src/main/java/com/huynd/skyobserver/services/PricesAPI.java
+++ app/src/main/java/com/huynd/skyobserver/services/PricesAPI.java
@@ -6,8 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.FieldMap;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -15,6 +16,8 @@ import retrofit2.http.Path;
  */
 
 public interface PricesAPI {
-    @GET("{carrier}/{srcPort}/{dstPort}")
-    Call<List<PricePerDayResponse>> getPricePerDay(@HeaderMap Map<String, String> headers, @Path("carrier") String carrier);
+    @POST("{carrier}/{srcPort}/{dstPort}")
+    Call<List<PricePerDayResponse>> getPricePerDay(@HeaderMap Map<String, String> headers,
+                                                   @FieldMap Map<String, String> postData,
+                                                   @Path("carrier") String carrier);
 }
--- app/src/main/java/com/huynd/skyobserver/utils/RequestHelper.java
+++ app/src/main/java/com/huynd/skyobserver/utils/RequestHelper.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Created by HuyND on 8/7/2017.
  */
 
-public class HeadersHelper {
+public class RequestHelper {
     public static Map<String, String> getDefaultHeaders() {
         Map<String, String> headers = new HashMap<>();
         headers.put("Host", Constants.HEADER_HOST);
@@ -24,5 +24,21 @@ public class HeadersHelper {
         headers.put("Accept-Language", Constants.HEADER_ACCEPT_LANGUAGE);
         headers.put("Cookie", Constants.COOKIES_DATA);
         return headers;
+    }
+
+    public static String requestHashBuilder(String srcPort, String dstPort, String carrier,
+                                            String strYear, String strMonth, String strDay) {
+        return carrier + "_" + srcPort + "_" + dstPort + "_" + strYear + strMonth + "01";
+    }
+
+    public static Map<String, String> requestDataBuilder(String strYear, String strMonth, String strDay) {
+        Map<String, String> data = new HashMap<>();
+        data.put("departureDate", strYear + "-" + strMonth + "-" + strDay);
+        data.put("returnDate", strYear + "-" + strMonth + "-" + strDay);
+        data.put("isRoundTrip", "false");
+        data.put("adultCount", "1");
+        data.put("childCount", "0");
+        data.put("infantCount", "0");
+        return data;
     }
 }
