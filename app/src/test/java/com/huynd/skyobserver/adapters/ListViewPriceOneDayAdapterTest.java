db72cf852470acc0bbc0e57ff27f3800dc287e79
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2017-09-10 20:19:07 +0700

    Story 05 - Add test to increace coverage for ListViewPriceOneDayAdapter class

---
+++ app/src/test/java/com/huynd/skyobserver/adapters/ListViewPriceOneDayAdapterTest.java
@@ -0,0 +1,41 @@
+package com.huynd.skyobserver.adapters;
+
+import android.view.View;
+
+import com.huynd.skyobserver.BuildConfig;
+import com.huynd.skyobserver.activities.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by HuyND on 9/10/2017.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = LOLLIPOP)
+public class ListViewPriceOneDayAdapterTest {
+    private MainActivity mActivity;
+
+    @Before
+    public void setUp() {
+        mActivity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void testGetViewWithNullItem() throws Exception {
+        ListViewPriceOneDayAdapter adapter = new ListViewPriceOneDayAdapter(mActivity);
+        adapter.add(null);
+        View view = adapter.getView(0, null, null);
+        ListViewPriceOneDayAdapter.ViewHolder vH = (ListViewPriceOneDayAdapter.ViewHolder) view.getTag();
+        assertEquals("", vH.getTextViewDepartTime().getText().toString());
+        assertEquals("", vH.getTextViewArriveTime().getText().toString());
+    }
+}
