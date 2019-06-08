c7c64e0b981a99c9ca50f8e761f790f70bd386c1
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2019-06-08 21:29:22 +0700

    Story 06 - Add test

--- app/src/androidTest/java/com/huynd/skyobserver/dagger/component/SkyObserverComponentAndroidTest.java
+++ app/src/androidTest/java/com/huynd/skyobserver/dagger/component/SkyObserverComponentAndroidTest.java
@@ -2,7 +2,8 @@ package com.huynd.skyobserver.dagger.component;
 
 import com.huynd.skyobserver.dagger.modules.ApiModuleAndroidTest;
 import com.huynd.skyobserver.fragments.ChooseOneDayAndroidTest;
-import com.huynd.skyobserver.fragments.ChooseOneDayWithCheapestPriceAndroidTest;
+import com.huynd.skyobserver.fragments.DateCheapestFragmentAndroidTest;
+import com.huynd.skyobserver.fragments.MonthCheapestFragmentAndroidTest;
 import com.huynd.skyobserver.fragments.PriceOneDayFragmentAndroidTest;
 import com.huynd.skyobserver.fragments.PricePerDayFragmentAndroidTest;
 import javax.inject.Singleton;
@@ -22,5 +23,7 @@ public interface SkyObserverComponentAndroidTest extends SkyObserverComponent {
 
     void inject(ChooseOneDayAndroidTest fragmentAndroidTest);
 
-    void inject(ChooseOneDayWithCheapestPriceAndroidTest fragmentAndroidTest);
+    void inject(DateCheapestFragmentAndroidTest fragmentAndroidTest);
+
+    void inject(MonthCheapestFragmentAndroidTest fragmentAndroidTest);
 }
